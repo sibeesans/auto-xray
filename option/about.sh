53db3fb58bd12795a4ba05b39d583b6ec0e07514
sibeesans <105978585+sibeesans@users.noreply.github.com>
2023-03-22 17:59:54 +0700

    x

---
+++ option/about.sh
@@ -0,0 +1,25 @@
+ echo -e "    
+ __     ___       _        
+ \ \   / / |_   _| | _____ 
+  \ \ / /| | | | | |/ / __|
+   \ V / | | |_| |   <\__ \
+    \_/  |_|\____|_|\_\___/
+    " | lolcat
+
+
+echo -e ">>>>>>>>>>Welcome to AutoScript Premium <<<<<<<<<<
+>>>>>>>>>>>>>>>>>>AUTOSCRIPT BY Vlukss<<<<<<<<<<<<<<<<<" | lolcat
+
+echo -e ">Support OS 
+For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+For Debian 9 & Debian 10 & Debian 11 " | lolcat
+
+echo -e "Contact Owner AutoScript
+> WA: 087854543721
+> TELEGRAM: t.me/mannnn_07" | lolcat
+
+echo -e "Thanks for your Order Vlukss AutoScript" | lolcat
+echo ""
+read -n 1 -s -r -p "  Press any key to Back to Menu"
+menu
+fi
